7cac7ec850325ee174fec6ff6e2a236513ea39a4
musicode <musicode@qq.com>
2015-08-10 11:04:28 +0800

    update

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ var es = require('event-stream');
 var amdDeploy = require('amd-deploy');
 var parseFile = require('amd-deploy/lib/parseFile');
 var resolveResourceId = require('amd-deploy/lib/resolveResourceId');
+var filePathToResourceId = require('amd-deploy/lib/filePathToResourceId');
 var resourceIdToFilePath = require('amd-deploy/lib/resourceIdToFilePath');
 var generateFileCode = require('amd-deploy/lib/generateFileCode');
 var parseFactoryResources = require('amd-deploy/lib/parseFactoryResources');
 var replaceResources = require('amd-deploy/lib/replaceResources');
+var readRequireConfig = require('amd-deploy/lib/readRequireConfig');
 var util = require('amd-deploy/lib/util');
 
 var htmlRules = [
@@ -107,6 +109,30 @@ function isAbsolute(url) {
 }
 
 /**
+ * 清掉 url 的 query 后缀
+ *
+ * @inner
+ * @param {string} url
+ * @return {string}
+ */
+function cleanQuery(url) {
+
+    var query = path.extname(url).split('?')[1];
+
+    if (query && query.length > 0) {
+
+        return url.substr(
+            0,
+            url.length - (query.length + 1)
+        );
+
+    }
+
+    return url;
+
+}
+
+/**
  * 正则替换
  *
  * @inner
@@ -116,11 +142,22 @@ function isAbsolute(url) {
  * @return {string}
  */
 function replaceByPattern(content, pattern, replacement) {
-    pattern = pattern.replace(/(\{|\}|\(|\)|\$|\.|\/)/g, '\\$1');
     return content.replace(
-        new RegExp(pattern, 'g'),
+        createPattern(pattern),
         replacement
     );
+}
+
+/**
+ * 创建一个正则表达式
+ *
+ * @inner
+ * @param {string} pattern
+ * @return {RegExp}
+ */
+function createPattern(pattern) {
+    pattern = pattern.replace(/(\{|\}|\(|\)|\[|\]|\$|\.|\/|\?)/g, '\\$1');
+    return new RegExp(pattern, 'g');
 }
 
 /**
@@ -153,7 +190,7 @@ function walkDependencies(file, rules) {
         if (results) {
             results.forEach(function (result) {
 
-                var dependencies = parser.match(result);
+                var dependencies = parser.match(result, file);
                 if (!dependencies) {
                     return;
                 }
@@ -172,27 +209,16 @@ function walkDependencies(file, rules) {
                     }
 
                     var raw = dependency.raw;
+                    var absolute = dependency.absolute;
 
-                    // 防止有 ?v=xxx 这种形式
-                    var query = path.extname(raw).split('?')[1];
-                    if (query && query.length > 0) {
-
-                        raw =
-                        dependency.raw =
-
-                        raw.substr(
-                            0,
-                            raw.length - (query.length + 1)
-                        );
-
+                    if (!absolute) {
+                        absolute = /^(?:\w|\.(?:\.)?)/.test(raw)
+                                 ? path.join(directory, raw)
+                                 : raw;
                     }
 
-                    if (!dependency.absolute) {
-                        dependency.absolute = /^(?:\w|\.(?:\.)?)/.test(raw)
-                                            ? path.join(directory, raw)
-                                            : raw;
-
-                    }
+                    dependency.raw = cleanQuery(raw);
+                    dependency.absolute = cleanQuery(absolute);
 
                     // 便于替换
                     dependency.text = result;
@@ -217,17 +243,18 @@ function walkDependencies(file, rules) {
  * 开发时通常会约定一些不通用的路径，如 ${staticServer}/src/a.js，编译时需要处理
  *
  * @inner
+ * @param {Object} file
  * @param {Array} dependencies
  * @param {Function} correct
  */
-function correctDependencies(dependencies, correct) {
+function correctDependencies(file, dependencies, correct) {
 
     if (!correct) {
         return;
     }
 
     for (var i = dependencies.length - 1; i >= 0; i--) {
-        correct(dependencies[i]);
+        correct(dependencies[i], file);
     }
 
 }
@@ -262,38 +289,120 @@ function filterDependencies(dependencies, filter) {
  * @inner
  * @param {Object} file
  * @param {Array} dependencies
- * @param {Function} replace
+ * @param {Function} rename
  */
-function replaceDependencies(file, dependencies, replace) {
+function renameDependencies(file, dependencies, rename) {
 
     var srcContent = file.contents.toString();
     var destContent = srcContent;
 
+    // 按 text 分组
+    var group = { };
+
     dependencies.forEach(function (dependency) {
+
+        var list = group[dependency.text];
+        if (!list) {
+            list = group[dependency.text] = [ ];
+        }
+
+        list.push(dependency);
+
+    });
+
+    each(group, function (text, dependencies) {
+
         destContent = replaceByPattern(
             destContent,
-            dependency.text,
+            text,
             function (result) {
 
-                var replacement = replace(dependency);
+                dependencies.forEach(function (dependency) {
 
-                if (replacement) {
-                    return replaceByPattern(
-                        result,
-                        dependency.raw,
-                        replacement
-                    );
-                }
+                    var replacement = rename(dependency);
+                    if (replacement) {
+                        result = replaceByPattern(
+                            result,
+                            dependency.raw,
+                            replacement
+                        );
+                    }
+
+                });
 
                 return result;
 
             }
         );
+
     });
 
     if (srcContent !== destContent) {
         file.contents = new Buffer(destContent);
     }
+
+}
+
+/**
+ * 获取递归计算的 md5
+ *
+ * @inner
+ * @param {string} dependency
+ * @param {Object} hashMap
+ * @param {Object} dependencyMap
+ * @return {string}
+ */
+function getRecursiveHash(dependency, hashMap, dependencyMap) {
+
+    // 递归分析出的完整的依赖列表
+    var dependencies = [ ];
+    var map = { };
+
+    var addDependency = function (dependency) {
+        // 要避免循环依赖
+        if (!map[dependency]) {
+
+            map[dependency] = 1;
+
+            dependencies.push(dependency);
+
+            var childDependencies = dependencyMap[dependency];
+            if (Array.isArray(childDependencies)) {
+                childDependencies.forEach(addDependency);
+            }
+
+        }
+    };
+
+    addDependency(dependency);
+
+    var hash = dependencies.map(
+        function (dependency) {
+
+            var hash = hashMap[dependency];
+
+            if (!hash && path.extname(dependency) !== '.html') {
+
+                if (path.relative(
+                        '/Users/zhujl/github/www-fe/asset',
+                        dependency
+                    ).indexOf('..') < 0
+                ) {
+                    console.log(
+                        '[hash not found]' + dependency,
+                        Object.keys(hashMap).length
+                    );
+                }
+
+            }
+
+            return hash || '';
+
+        }
+    )
+    .join('');
+
+    return hash ? md5(new Buffer(hash)) : '';
 
 }
 
@@ -303,10 +412,10 @@ function replaceDependencies(file, dependencies, replace) {
  * @param {Object} options
  * @property {Array} options.htmlRules
  * @property {Array} options.cssRules
- * @property {Object} options.amdConfig
+ * @property {Function} options.getAmdConfig 获取 AMD 配置，会传入当前处理的文件路径
  * @property {Function} options.renameFile 重命名文件，比如加上 md5
+ * @property {Function} options.renameDependency 重命名依赖，比如加上 md5
  * @property {Function=} options.filterDependency 过滤非法依赖，比如 src/img/${a}.jpg
- * @property {Function=} options.renameDependency 重命名依赖，按 md5 进行替换时可以自定义替换规则
  * @property {Function} options.correctDependency 纠正依赖的格式，比如开发约定 {{ $static_server }}/src 开头
  *                                                build 需要纠正为正确的路径
  */
@@ -350,7 +459,7 @@ Resource.prototype = {
      *
      * @param {Object} options
      * @property {Function} options.process
-     * @property {Function} options.replace
+     * @property {Function} options.rename
      */
     htmlDependencies: function (options) {
 
@@ -364,6 +473,7 @@ Resource.prototype = {
             );
 
             correctDependencies(
+                file,
                 dependencies,
                 me.correctDependency
             );
@@ -377,20 +487,18 @@ Resource.prototype = {
                 options.process(file, dependencies);
             }
 
-            if (options.replace) {
-
-                replaceDependencies(
+            if (options.rename) {
+                renameDependencies(
                     file,
                     dependencies,
                     function (dependency) {
-                        return options.replace(
+                        return options.rename(
                             dependency,
                             me.hashMap,
                             me.dependencyMap
                         );
                     }
                 );
-
             }
 
             callback(null, file);
@@ -403,7 +511,7 @@ Resource.prototype = {
      *
      * @param {Object} options
      * @property {Function} options.process
-     * @property {Function} options.replace
+     * @property {Function} options.rename
      */
     cssDependencies: function (options) {
 
@@ -417,6 +525,7 @@ Resource.prototype = {
             );
 
             correctDependencies(
+                file,
                 dependencies,
                 me.correctDependency
             );
@@ -430,13 +539,13 @@ Resource.prototype = {
                 options.process(file, dependencies);
             }
 
-            if (options.replace) {
+            if (options.rename) {
 
-                replaceDependencies(
+                renameDependencies(
                     file,
                     dependencies,
                     function (dependency) {
-                        return options.replace(
+                        return options.rename(
                             dependency,
                             me.hashMap,
                             me.dependencyMap
@@ -456,29 +565,19 @@ Resource.prototype = {
      *
      * @param {Object} options
      * @property {Function} options.process
-     * @property {Function} options.replace
+     * @property {Function} options.rename
      */
     amdDependencies: function (options) {
 
         var me = this;
 
-        var replace = options.replace;
-
-        var config = { };
-        extend(config, me.amdConfig);
-
-        if (replace) {
-            config.replaceRequireResource = function (raw, absolute) {
-                return replace({
-                    raw: raw,
-                    absolute: absolute
-                });
-            };
-        }
+        var replace = options.rename;
 
         return es.map(function (file, callback) {
 
             var dependencies = [ ];
+
+            var config = me.getAmdConfig(file.path);
 
             var fileInfo = parseFile(
                 file.path,
@@ -519,6 +618,7 @@ Resource.prototype = {
             );
 
             correctDependencies(
+                file,
                 dependencies,
                 me.correctDependency
             );
@@ -533,6 +633,13 @@ Resource.prototype = {
             }
 
             if (replace) {
+
+                config.replaceRequireResource = function (raw, absolute) {
+                    return replace({
+                        raw: raw,
+                        absolute: absolute
+                    });
+                };
 
                 replaceResources(
                     fileInfo,
@@ -549,7 +656,6 @@ Resource.prototype = {
 
         });
     },
-
 
     /**
      * 分析文件的 hash
@@ -585,7 +691,6 @@ Resource.prototype = {
      *
      * @param {Object} options
      * @property {string} options.type
-     * @property {Function} options.correctDependency
      */
     analyzeFileDependencies: function (options) {
 
@@ -596,15 +701,7 @@ Resource.prototype = {
 
                 me.dependencyMap[file.path] = dependencies.map(
                     function (dependency) {
-
-                        if (options.correctDependency) {
-                            dependency = options.correctDependency(
-                                dependency
-                            );
-                        }
-
                         return dependency.absolute;
-
                     }
                 );
 
@@ -619,7 +716,6 @@ Resource.prototype = {
      * @param {Object} options
      * @property {string} options.type
      * @property {Function} options.customReplace
-     * @property {Function} options.correctDependency
      */
     replaceFileDependencies: function (options) {
 
@@ -627,50 +723,8 @@ Resource.prototype = {
         var hashMap = me.hashMap;
         var dependencyMap = me.dependencyMap;
 
-        var renameFile = me.renameFile;
-        var renameDependency = me.renameDependency;
-
-        var getRecursiveHash = function (dependency) {
-
-            // 递归分析出的完整的依赖列表
-            var allDependencies = [];
-
-            var recursive = function (dependency) {
-                var childDependencies = dependencyMap[dependency];
-                if (Array.isArray(childDependencies)) {
-                    merge(
-                        allDependencies,
-                        childDependencies
-                    );
-                    childDependencies.forEach(recursive);
-                }
-            };
-
-            recursive(dependency);
-
-            var hash = allDependencies.map(
-                function (dependency) {
-                    return hashMap[dependency] || '';
-                }
-            )
-            .join('');
-
-            return hash ? md5(new Buffer(hash)) : '';
-
-        };
-
         return me[options.type + 'Dependencies']({
             process: function (file, dependencies) {
-
-                // 首先自己必须有 hash
-                if (!hashMap[file.path]) {
-                    return;
-                }
-
-                var hash = getRecursiveHash(file.path);
-                if (hash) {
-                    file.path = renameFile(file, hash);
-                }
 
                 if (options.customReplace) {
                     var srcContent = file.contents.toString();
@@ -680,21 +734,130 @@ Resource.prototype = {
                     }
                 }
 
+                file.path = me.renameFile(
+                    file,
+                    getRecursiveHash(file.path, hashMap, dependencyMap)
+                );
+
             },
-            replace: function (dependency) {
+            rename: function (dependency) {
 
-                if (options.correctDependency) {
-                    dependency = options.correctDependency(dependency);
-                }
-
-                var hash = getRecursiveHash(dependency.absolute);
-                if (hash) {
-                    return renameDependency(dependency, hash);
-                }
+                return me.renameDependency(
+                    dependency,
+                    getRecursiveHash(dependency.absolute, hashMap, dependencyMap)
+                );
 
             }
         });
 
+    },
+
+    /**
+     * 编译 amd 模块
+     */
+    buildAmdModules: function () {
+
+        var me = this;
+
+        return es.map(function (file, callback) {
+
+            amdDeploy({
+                file: file.path,
+                content: file.contents.toString(),
+                config: me.getAmdConfig(file.path),
+                callback: function (code) {
+
+                    file.contents = new Buffer(code);
+
+                    callback(null, file);
+
+                }
+            });
+
+        });
+
+    },
+
+    /**
+     * 解析 amd 依赖
+     *
+     * @param {string} filePath 文件路径
+     * @param {string} match 文件中匹配到的原始字符串
+     * @param {string} literal 从 match 中抽离出的符合 id 规则的字面量
+     * @return {Array.<string>}
+     */
+    parseAmdDependencies: function (filePath, match, literal) {
+
+        // literal 可能是 'moduleId'、'[ "module1", "module2" ]'、xxx（非法 js 变量）
+
+        literal = literal.trim();
+
+        var resources;
+
+        try {
+            var factory = new Function('return ' + literal);
+            resources = factory();
+        }
+        catch (e) {
+            console.error('[INFO][amd id parse error]' + match);
+            resources = literal;
+        }
+
+        if (!resources) {
+            return;
+        }
+
+        if (!Array.isArray(resources)) {
+            resources = [ resources ];
+        }
+
+        var me = this;
+        var config = me.getAmdConfig(filePath);
+
+        var result = [ ];
+
+        resources.forEach(function (resourceId) {
+
+            var filePath = resourceIdToFilePath(
+                resourceId,
+                config
+            );
+
+            if (filePath) {
+                result.push({
+                    amd: true,
+                    raw: resourceId,
+                    absolute: filePath
+                });
+            }
+
+        });
+
+        return result;
+
+    },
+
+    /**
+     * 解析文本中的 require.config，比如写在 html
+     *
+     * @param {string} content
+     * @return {Array}
+     */
+    parseAmdConfig: function (content) {
+        return readRequireConfig(content);
+    },
+
+    /**
+     * 文件路径转为资源 ID
+     *
+     * @param {string} filePath
+     * @return {string}
+     */
+    filePathToResourceId: function (filePath) {
+        return filePathToResourceId(
+            filePath,
+            this.getAmdConfig(filePath)
+        );
     }
 
 };
